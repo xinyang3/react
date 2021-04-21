254f2d7b5b79bd0c072fb2083fe7bab26b1021da
xinyang3 <xinyang.zhu@outlook.com>
2021-04-21 10:12:35 +0800

    postList

--- redux-essentials-example-app/src/features/posts/postsList.jsx
+++ redux-essentials-example-app/src/features/posts/postsList.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
 
 const PostsList = () => {
   const posts = useSelector((state) => state.posts)
-  const renderPosts = posts.map((post) => (
+  const orderedPosts = posts
+    .slice()
+    .sort((a, b) => b.date.localeCompare(a.date))
+  const renderPosts = orderedPosts.map((post) => (
     <article className="post-excert" key={post.id}>
       <h3>{post.title}</h3>
       <p className="post-content">{post.content.substring(0, 100)}</p>
+      <Link to={`/posts/${post.id}`} className="button muted-button">
+        view post
+      </Link>
     </article>
   ))
 
